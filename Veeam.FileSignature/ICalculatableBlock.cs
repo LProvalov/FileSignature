f19cc2cd8e5edfc007048d4fb90421a982d0e826
Lev Provalov <lev.provalov@swtecnn.com>
2021-06-24 16:29:29 +0300

    Start implementation of basic class architecture

---
+++ Veeam.FileSignature/ICalculatableBlock.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Veeam.FileSignature
+{
+    internal interface ICalculatableBlock
+    {
+        void Calculate();
+    }
+}
---
+++ Veeam.FileSignature/SHA256Block.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Veeam.FileSignature
+{
+    internal class SHA256Block : ICalculatableBlock
+    {
+        private int _blockNumber;
+        private byte[] _hashValue;
+        private Stream _dataStream;
+
+        public SHA256Block(int blockNumber, Stream dataStream)
+        {
+            _blockNumber = blockNumber;
+            _dataStream = dataStream;
+        }
+
+        public void Calculate()
+        {
+            if (_dataStream.CanRead)
+            {
+
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    try
+                    {
+                        _hashValue = sha256.ComputeHash(_dataStream);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"", ex);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception($"{_blockNumber}, Can't read data stream.");
+            }
+        }
+    }
+}
